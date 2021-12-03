83dadabad5b78aae86adc127a3daf51df4f88b30
ej0306 <54604143+ej0306@users.noreply.github.com>
2021-12-03 15:27:08 -0500

    deleted

--- cunyzeroweb/cunyzerofend/static/js/register.js
+++
@@ -1,2 +0,0 @@
-src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"
-new mdc.textField.MDCTextField(document.querySelector(".text-field-fill-leading-custom-Qolay1 .mdc-text-field"));
