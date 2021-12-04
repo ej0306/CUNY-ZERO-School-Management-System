23dbfd8bc295928a675c03b4acd634767ce33e5a
ej0306 <54604143+ej0306@users.noreply.github.com>
2021-12-04 16:09:27 -0500

    updated

--- general/urls.py
+++ general/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='general-home'),
     path('about/', views.about, name='general-about'),
+    path('home/', views.home2, name='home2'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('searchclasses/', views.searchclasses, name='searchclasses'),
+    path('about/', views.about, name='about'),
+    path('profile2/', views.profile2, name='profile2'),
+
 ]
+
--- general/views.py
+++ general/views.py
@@ -5,6 +5,27 @@ from courses.models import Course, Result, ReviewClasses
 
 
 # Create your views here.
+def home2(request):
+    return render(request, 'general/homepage.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def searchclasses(request):
+    return render(request,'searchclasses.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def profile2(request):
+    return render(request,'general/profile2.html')
+
 def home(request):
     courses = Course.objects.all()
     students = Result.objects.filter(cgpa__gte=3.5)
