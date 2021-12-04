86c95e5ff438e8ab8ceedbd3e676aba0471056ff
ej0306 <54604143+ej0306@users.noreply.github.com>
2021-12-04 13:24:20 -0500

    updated

--- cunyzeroweb/cunyzerofend/static/js/profile.js
+++
@@ -1,2 +0,0 @@
-src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js
-new mdc.textField.MDCTextField(document.querySelector(".text-field-fill-leading-custom-tkVWbB .mdc-text-field"));
