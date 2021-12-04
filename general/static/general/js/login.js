8a52e342620a77eedf6f14504d0ef11798d8bdb6
ej0306 <54604143+ej0306@users.noreply.github.com>
2021-12-04 16:13:02 -0500

    filed updated

--- general/static/general/js/login.js
+++ general/static/general/js/login.js
@@ -1,13 +1,13 @@
 src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"
 src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.6.1/dist/simpleParallax.min.js"
-new mdc.textField.MDCTextField(document.querySelector(".text-field-fill-leading-custom-b8DrbD .mdc-text-field"));
+new mdc.textField.MDCTextField(document.querySelector(".text-field-fill-leading-custom .mdc-text-field"));
 window.addEventListener("load", function () {
-              var image = document.querySelectorAll(".cuny-TQOkgU");
-              new simpleParallax(image, {
-                orientation: "left",
-                scale: 1.2,
-                overflow: false,
-                delay: 0.4,
-                transition: "none",
+                var image = document.querySelectorAll(".cuny");
+                new simpleParallax(image, {
+                  orientation: "left",
+                  scale: 1.2,
+                  overflow: false,
+                  delay: 0.4,
+                  transition: "none",
+                });
               });
-            });
