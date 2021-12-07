8cbf6c62d9211ab96d4a6039dd52f2e63c95dcf2
ej0306 <54604143+ej0306@users.noreply.github.com>
2021-12-06 22:54:46 -0500

    updated

--- general/urls.py
+++ general/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from general import views
+from users import views as user_views
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('', views.home, name='general-home'),
-    path('about/', views.about, name='general-about'),
-    path('contact/', views.contact, name='contact'),
-    path('loginbase/',views.loginbase, name='loginbase'),
-    path('register/', views.base_page, name='register'),
-    path('searchclasses/', views.base_page, name='searchclasses'),
-    path('about/', views.about, name='about'),
-    path('baseprofile/', views.base_profile, name='baseprofile'),
-    path('basepage/', views.base_page, name='basepage'),
-    path('login/', views.login,  name='login'),
+    path('login/', views.login, name='login'),
+    path('registerInstructor/', views.registerinstructor, name='registerInstructor'),
+    path('registerStudent/', views.registerStudent, name='registerStudent'),
 ]
 
--- general/views.py
+++ general/views.py
@@ -1,33 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import views
 from courses.models import Course, Result, ReviewClasses
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'general/homepage.html',{'title': 'Home'})
+    return render(request, 'general/home.html',{'title': 'Home'})
 
-def base_page(request):
-    return render(request, 'general/base-page.html',{'title': 'Base Page'})
+def registerinstructor(request):
+    return render(request, 'registerInstructor.html',{'title': 'registerInstructor'})
 
-def contact(request):
-    return render(request,'general/contact.html',{'title': 'Contact'})
-
-def searchclasses(request):
-    return render(request,'general/base-page.html',{'title': 'SearchClasses'})
-
-def loginbase(request):
-    return render(request,'general/loginbase.html',{'title': 'LogIn'})
-
-def register(request):
-    return render(request,'general/base-page.html',{'title': 'Register'})
-
-def base_profile(request):
-    return render(request,'general/base-profile.html',{'title': 'Profile'})
-
-def about(request):
-    return render(request, 'general/about.html',{'title': 'About'})
+def registerStudent(request):
+    return render(request, 'registerStudent.html',{'title': 'registerStudent'})
 
 def login(request):
-    return render(request,'login.html')
+    return render(request, 'login.html',{'title': 'login2'})
