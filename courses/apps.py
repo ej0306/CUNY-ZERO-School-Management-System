8928afe476ff1cd29a27ad61458a60aa726e3e09
Kelvin <54338129+Breathing-ls-Fun@users.noreply.github.com>
2021-12-10 03:21:26 -0500

    fixed requirements.txt

--- courses/apps.py
+++ courses/apps.py
@@ -5,6 +5,7 @@ class CoursesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'courses'
 
-    def ready(self):
-        from .updater import start
-        start()
+    # uncomment after migrations if want to do the checks at the end of class registration period
+    # def ready(self):
+    #     from .updater import start
+    #     start()
